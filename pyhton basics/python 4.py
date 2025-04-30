75a9fffd99c516800492ab64c4e71c6922a1c96d
nirma9 <nirmalaborana1@gmail.com>
2025-04-30 22:47:38 +0530

    python basics

---
+++ pyhton basics/python 4.py
@@ -0,0 +1,21 @@
+# print("Hello World")
+# print("HII, I am Here")
+
+# print("I am nirma")
+# print("i am from india \niam 28 years old.....")
+
+# print(2+3)
+# print(4*6)
+# print(4-9)
+
+# age = 98
+# print(age)
+
+# import turtle
+# t = turtle.Turtle()
+# t.forward(500)
+
+name = input("what is your name")
+print("my name is ",name)
+
+
