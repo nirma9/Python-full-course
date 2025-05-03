e3cf2e01cdfac4add8d6859248e58fafe5d47b1c
nirma9 <nirmalaborana1@gmail.com>
2025-05-03 16:11:21 +0530

    sequence data types

---
+++ sequencedatatypes/seq.py
@@ -0,0 +1,15 @@
+# lists = ["apple","banana","orange","grapes","mango"]
+# print(lists[1])
+# print(lists[0])
+# lists.append("watermelon")
+# print(lists)
+# print(lists)
+
+colors = ("red","blue","green","yellow")
+print(colors)
+print(colors[2])
+
+for i in range(1,9,2):
+               print(i)
+for i in range(0,91,2):
+        print(i)
