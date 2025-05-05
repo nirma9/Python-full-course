837e7aa4b81bab984fb9209007e8c90e7abd2bd9
nirma9 <nirmalaborana1@gmail.com>
2025-05-05 12:24:06 +0530

    comments in  python

---
+++ comments/comments.py
@@ -0,0 +1,28 @@
+# print("single line comments")
+#this is addition function 
+# a = 9
+# b = 8
+# print(a+b)
+
+#triple quotes string
+
+'''
+this is one multiple line comments ..
+that si used with triple quoted string
+so this si the eample of multiple line comments
+dsgfd
+
+dfadsfasd
+f
+asdfdsf
+
+fdasdf'''
+
+"""
+fsadfa
+dsfasdfda
+dsfadsfas
+dsfaadsfasd
+fdsfaasdf"""
+print("i am from india")
+
