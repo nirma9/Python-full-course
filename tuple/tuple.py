2596427cc246aab39f5475e058ef442048f50260
nirma9 <nirmalaborana1@gmail.com>
2025-05-08 13:59:59 +0530

    files added

---
+++ tuple/tuple.py
@@ -0,0 +1,12 @@
+my_tuple = (1,2,3)
+mix_tup = ("sanju",65,True)
+single = (6,)
+print(type(single))
+t = (10,20,30)
+print(t[1])
+x,y,z = t
+print(x,y,z)
+print(x)
+#slicing
+print(t[0:2])
+print(dir(my_tuple))
