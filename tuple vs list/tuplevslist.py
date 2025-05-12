e38b36bef7078f0472850bfd99e9fc62cf546de4
nirma9 <nirmalaborana1@gmail.com>
2025-05-12 16:09:28 +0530

    added files

---
+++ tuple vs list/tuplevslist.py
@@ -0,0 +1,20 @@
+#list
+# my_list =[10,20,30,40]
+# print(my_list)
+# print(my_list[2])
+# my_list.append(50)
+# print(my_list)
+# my_list.remove(20)
+# print(my_list)
+# for item in my_list:
+#                print(item)
+
+
+#tuple
+my_tuple = (10,20,30,40)
+print(my_tuple)
+
+print(my_tuple[1])
+# my_tuple.append(40)
+print(my_tuple.count(20))
+
---
+++ typeconversion/typeconversion.py
@@ -0,0 +1,29 @@
+# a = 5
+# b = 2.5
+# c = a+b
+# print(c)
+# print(type(c))
+
+#explicit type conversion
+x = 7.8
+y = int(x)
+print(y)
+print(type(y))
+
+# int to string
+num = 10
+s = str(num)
+print(s)
+print(type(s))
+
+
+
+
+#list to set
+lit = [1,2,3,4]
+st = set(lit)
+print(st)
+
+
+age = input("enter your age: ")
+print("in 5 years you will be ", age + 5)
