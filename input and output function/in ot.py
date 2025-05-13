1792d2f05e002b8f586704c3d378d1681c5b4b07
nirma9 <nirmalaborana1@gmail.com>
2025-05-13 16:09:52 +0530

    input output added

---
+++ input and output function/in ot.py
@@ -0,0 +1,12 @@
+# name = input("enter your name: ")
+# age = int(input("enter your sge:"))
+# print(age +5)
+
+# print("my age is :", age)
+# print("python","programming",sep = "->",end ="\n")
+
+name = input("enter your name: ")
+print("good morning", name)
+
+
+
