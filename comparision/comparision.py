d6efb953520fc6800334df3ddc1881117c47d3c1
nirma9 <nirmalaborana1@gmail.com>
2025-05-15 11:05:59 +0530

    comprision

---
+++ comparision/comparision.py
@@ -0,0 +1,39 @@
+# a = 8
+# b= 8
+# print(a==b)
+# a = "apple"
+# b = "orange"
+# print(a == b)
+#not equal to 
+
+# age1 = 67
+# age2 = 78
+# print(age1 != age2)
+
+# color = "blue"
+# fav = "blue"
+# print(color!=fav)
+
+#greater than
+
+# score = 80
+# passing = 50
+# print(score > passing)
+
+# #less than
+# price = 20
+# budget = 50
+# print(price < budget)
+
+#>= greater than equal to 
+# marks = 65
+# minim = 60
+# print(marks >= minim)
+
+age = int(input("enter your age"))
+if age == 18:
+               print("you are in 18")
+elif age < 18:
+        print("you are minor")
+else:
+        print("you are older")
---
+++ operators/operators.py
@@ -0,0 +1,22 @@
+# a = 10
+# b = 5
+# print("addition: ", a+b)
+# print("sub:",a-b)
+# print("multi:", a*b)
+# print("modulus", a%b)
+# print("floor division",a//b)
+# print("expon:", a**b)
+
+#data anlytics
+# marks = 78
+# marks2 = 89
+# marks3 = 78
+# avg = (marks+marks2+marks3)/3
+# print(avg)
+
+#chk even or odd number
+num = int(input("enter any number:"))
+if num % 2==0:
+               print("even")
+else:
+        print("odd number")
