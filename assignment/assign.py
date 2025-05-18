b3444eb11b4ce56363f47a99adcbe401b4c65629
nirma9 <nirmalaborana1@gmail.com>
2025-05-18 22:03:48 +0530

    assignment operators

---
+++ assignment/assign.py
@@ -0,0 +1,50 @@
+# x = 10
+# print(x)
+
+# #add and assign
+# x = x+5
+# x+=5
+# x+=3
+# print(x)
+
+#subtract and assign
+# x = 10
+# x-=4
+# print(x)
+# # x = x-4
+
+# #multiply and assign
+# x*=3
+# print(x)
+
+
+#divide and assign
+# x = 20
+# x/=4
+# print(x)
+
+# #modulus and assign
+# x = 10
+# x%=4
+# print(x)
+
+# #floor divide and assign
+# x = 9
+# x//=2
+# print(x)
+
+# **=power and assign
+# x = 2
+# x**=3
+# print(x)
+
+
+balance = 1000
+balance += 500    #deposit
+print(balance)
+balance -= 200      #withdraw
+print(balance)
+balance *= 1.05        #interest 5%
+print(balance)
+
+
